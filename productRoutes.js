b7fae9b60e98a6d35baf0b08c3fbebc7be018222
hasshamkhanswabi032-ctrl <hasshamkhanswabi032@gmail.com>
2026-03-11 00:57:03 +0500

    Add files via upload

---
+++ productRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getProducts,
+    createProduct,
+} = require('../controllers/productController');
+const { protect, admin } = require('../middleware/authMiddleware');
+
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id').delete(protect, admin, deleteProduct);
+
+module.exports = router;
